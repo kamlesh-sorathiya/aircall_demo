f3043f7ea549e7795060c80e81b961446e62aa20
Kamlesh Sorathiya <kamleshs@Kamleshs-MacBook-Pro.local>
2024-01-04 17:39:35 +0530

    build settings

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebPackPlugin({
-      template: "./public/index.html",
+      template: "./dist/index.html",
       filename: "./index.html"
     })
   ]
